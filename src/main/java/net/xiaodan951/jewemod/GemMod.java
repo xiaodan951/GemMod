acf3ff6891ad9cdd7296895f2706d5179a2a3cf6
xiaodan951 <3148204468@qq.com>
2021-11-30 22:49:45 +0800

    mod

--- src/main/java/net/xiaodan951/jewemod/GemMod.java
+++ src/main/java/net/xiaodan951/jewemod/GemMod.java
@@ -18,11 +18,9 @@ import net.xiaodan951.jewemod.item.ModItems;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import java.util.stream.Collectors;
-
 // The value here should match an entry in the META-INF/mods.toml file
-@Mod(JeweMod.MOD_ID)
-public class JeweMod
+@Mod(GemMod.MOD_ID)
+public class GemMod
 {
 
     public static final String MOD_ID = "jewemod";
@@ -30,7 +28,7 @@ public class JeweMod
     // Directly reference a log4j logger.
     private static final Logger LOGGER = LogManager.getLogger();
 
-    public JeweMod() {
+    public GemMod() {
         // Register the setup method for modloading
         IEventBus eventBus = FMLJavaModLoadingContext.get().getModEventBus();
 
--- src/main/java/net/xiaodan951/jewemod/item/ModItems.java
+++ src/main/java/net/xiaodan951/jewemod/item/ModItems.java
@@ -6,13 +6,12 @@ import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
-import net.xiaodan951.jewemod.JeweMod;
+import net.xiaodan951.jewemod.GemMod;
 
 public class ModItems
 {
 
-    public static final DeferredRegister <Item> ITEMS =
-            DeferredRegister.create(ForgeRegistries.ITEMS, JeweMod.MOD_ID);
+    public static final DeferredRegister <Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, GemMod.MOD_ID);
 
     public static final RegistryObject<Item> WHITE_JADE = ITEMS.register("white_jade", () -> new Item(new Item.Properties().group(ItemGroup.MATERIALS)));
     public static final RegistryObject<Item> TOPAZ = ITEMS.register("topaz", () -> new Item(new Item.Properties().group(ItemGroup.MATERIALS)));
