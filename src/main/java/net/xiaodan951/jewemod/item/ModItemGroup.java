cfed2f76d34d8d973e1f4063e56b515da343b10b
xiaodan951 <3148204468@qq.com>
2021-11-30 23:10:13 +0800

    mod

---
+++ src/main/java/net/xiaodan951/jewemod/item/ModItemGroup.java
@@ -0,0 +1,16 @@
+package net.xiaodan951.jewemod.item;
+
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemStack;
+
+public class ModItemGroup
+{
+
+    public static final ItemGroup GEM_MOD = new ItemGroup("gemModTab") {
+        @Override
+        public ItemStack createIcon() {
+            return new ItemStack(ModItems.WHITE_JADE.get());
+        }
+    };
+
+}
--- src/main/java/net/xiaodan951/jewemod/item/ModItems.java
+++ src/main/java/net/xiaodan951/jewemod/item/ModItems.java
@@ -13,8 +13,8 @@ public class ModItems
 
     public static final DeferredRegister <Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, GemMod.MOD_ID);
 
-    public static final RegistryObject<Item> WHITE_JADE = ITEMS.register("white_jade", () -> new Item(new Item.Properties().group(ItemGroup.MATERIALS)));
-    public static final RegistryObject<Item> TOPAZ = ITEMS.register("topaz", () -> new Item(new Item.Properties().group(ItemGroup.MATERIALS)));
+    public static final RegistryObject<Item> WHITE_JADE = ITEMS.register("white_jade", () -> new Item(new Item.Properties().group(ModItemGroup.GEM_MOD)));
+    public static final RegistryObject<Item> TOPAZ = ITEMS.register("topaz", () -> new Item(new Item.Properties().group(ModItemGroup.GEM_MOD)));
 
 
     public static void register(IEventBus eventBus)
