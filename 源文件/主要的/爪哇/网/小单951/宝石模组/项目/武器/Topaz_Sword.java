d43e20aa4faa9710bf5b45109f007a3683566b58
xiaodan951 <91748927+xiaodan951@users.noreply.github.com>
2021-12-12 09:38:03 +0800

    Update and rename src/main/java/net/xiaodan951/gemmod/items/weapons/Topaz_Sword.java to 源文件/主要的/爪哇/网/小单951/宝石模组/项目/武器/Topaz_Sword.java

---
+++ 源文件/主要的/爪哇/网/小单951/宝石模组/项目/武器/Topaz_Sword.java
@@ -0,0 +1,10 @@
+package net.xiaodan951.gemmod.items.weapons;
+
+import net.minecraft.item.SwordItem;
+import net.xiaodan951.gemmod.group.ModGroup;
+
+public class Topaz_Sword extends SwordItem {
+    public Topaz_Sword () {
+        super(ModItemTier.TOPAZ, 4, -3,new Properties().group(ModGroup.GemmodGroup));
+    }
+}
